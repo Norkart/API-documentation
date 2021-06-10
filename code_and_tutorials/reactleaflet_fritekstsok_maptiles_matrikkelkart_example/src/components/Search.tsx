00713bfda1ec3dc5a43e7a2f85886e2c56785406
Lars Martin Aders <larade@norkart.no>
2021-06-10 13:59:22 +0200

    search

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Search.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Search.tsx
@@ -1,53 +1,61 @@
 import { TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react'
-import { useRecoilValue } from 'recoil';
-import { apiKey } from '../state/state';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { Address, apiKey, selectedAddress } from '../state/state';
 
 export const Search = () => {
     
     
     const [query, setQuery] = useState<string | null>(null);
-    const apiKeyState = useRecoilValue<string | null>(apiKey);
-    const suggestions = useRecoilValue<string | null>(apiKey);
+    const [suggestedAdress, setSuggestedAddresses] = useState<Address[] | null>(null);
+    const apiKeyState = useRecoilValue<string>(apiKey);
+    const setAddress = useSetRecoilState<Address>(selectedAddress);
 
     useEffect(() => {
-        /*
+        
         var fritekstQuery = `https://www.webatlas.no/WAAPI-FritekstSok/suggest/kommunecustom?Query=${query}&Size=5&Targets=gateadresse`; 
 
         async function fetchFritekst(){
-            var suggestion = new Array<string>();
-            const apiResult = await fetch(
-                `${fritekstQuery}`,
-                {
-                    headers: {
-                    'Accept': 'application/json',
-                    'X-WAAPI-TOKEN': `${apiKeyState}`
-                    },
-                    method: "GET"
+            if(query){
+                var suggestions = new Array<Address>();
+                const apiResult = await fetch(
+                    `${fritekstQuery}`,
+                    {
+                        headers: {
+                        'Accept': 'application/json',
+                        'X-WAAPI-TOKEN': `${apiKeyState}`
+                        },
+                        method: "GET"
+                    }
+                    );
+        
+                if(apiResult.status === 200){
+                    var json = await apiResult.json();
+                    json.Options.forEach((suggestion: any) => {
+                        suggestions.push({id: suggestion.Id,  text: suggestion.Text, latLng: {lat: suggestion.PayLoad.Posisjon.X, lng: suggestion.PayLoad.Posisjon.Y}})
+                    });
+                    setSuggestedAddresses(suggestions);
                 }
-                );
-    
-            if(apiResult.status === 200){
-                var json = await apiResult.json();
-                json.Options.forEach((suggestion: any) => {
-                    suggestions.push({id: suggestion.Id, adresseMatrikkelid: suggestion.PayLoad.AdresseMatrikkelNummer, text: suggestion.Text, latlng: {lat: suggestion.PayLoad.Posisjon.X, lng: suggestion.PayLoad.Posisjon.Y}})
-                });
-                return suggestions;
             }
         }
-        */
+        fetchFritekst();
+        
     }, [query])
 
     return (
         <div>
-            <form noValidate autoComplete="off">
+            {apiKeyState != "" &&
+            <div>
+                <form noValidate autoComplete="off">
                 <TextField
                     id="standard-basic"
                     label="Søk opp adresse"
                     onChange={(e) => setQuery(e.target.value)}
             />
             </form>
-            {query && <p>{query}</p>}
+                {suggestedAdress && suggestedAdress.map((address : Address) => <p onClick={setAddress.bind(this, address)}>{address.text}</p>)}
+            </div>                 
+            }
         </div>
     )
 }
