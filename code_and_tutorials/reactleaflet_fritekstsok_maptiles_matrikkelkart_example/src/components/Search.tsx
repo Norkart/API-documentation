3385124b2a4075b6bd0987c2b6dd97640510c7f3
Lars Martin Aders <larade@norkart.no>
2021-06-10 13:34:50 +0200

    search

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Search.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Search.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { TextField } from '@material-ui/core';
+import React, { useState } from 'react'
 
 export const Search = () => {
+    
+    
+    const [query, setQuery] = useState<string | null>(null);
+
+    
     return (
         <div>
-            
+            <form noValidate autoComplete="off">
+                <TextField
+                    id="standard-basic"
+                    label="Søk opp adresse"
+                    onChange={(e) => setQuery(e.target.value)}
+            />
+            </form>
+            {query && <p>{query}</p>}
         </div>
     )
 }
