0550fae157746a5cc2105058f41a63c04ad8cffc
Kari Meling Johannessen <karjoh@norkart.no>
2021-06-10 14:06:16 +0200

    add map

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/App.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/App.tsx
@@ -3,12 +3,14 @@ import { ApiKey } from './components/ApiKey'
 import logo from './logo.svg';
 import './App.css';
 import { Search } from './components/Search';
+import { Map } from './components/Map';
 
 function App() {
   return (
     <div className="App">
       <ApiKey />
       <Search />
+      <Map/>
      content here
     </div>
   );
