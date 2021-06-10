7190f34133408e573af45188c806cf2e5c2d808f
Kari Meling Johannessen <karjoh@norkart.no>
2021-06-10 14:10:23 +0200

    norkart tiles

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Map.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Map.tsx
@@ -10,13 +10,14 @@ interface Props {
 export const Map = (props: Props) => {
     const position: LatLngExpression = [51.505, -0.09]
     const apikey = useRecoilValue(apiKey)
+    console.log(apikey)
 
     return (
         <div>
             {apikey &&
                 <MapContainer style={{ height: '100vh', width: '100vw' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    url={`https://waapi.webatlas.no/maptiles/tiles/webatlas-orto-newup/wa_grid/{z}/{x}/{y}.jpeg?APITOKEN=${apikey}`}
                 />
                 <Marker position={[51.505, -0.09]}>
                     <Popup>
