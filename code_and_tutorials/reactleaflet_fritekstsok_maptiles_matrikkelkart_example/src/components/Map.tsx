b49d592f4bc45f383bca4e259b50125f6fb41419
Kari Meling Johannessen <karjoh@norkart.no>
2021-06-10 14:22:23 +0200

    usestate

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Map.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/Map.tsx
@@ -1,30 +1,29 @@
 import { LatLngExpression } from "leaflet"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import { useRecoilValue } from "recoil"
-import { apiKey } from "../state/state"
+import { useRecoilValue, useRecoilValueLoadable } from "recoil"
+import { apiKey, position, selectedAddress, selectedTeig } from "../state/state"
 
 interface Props {
 
 }
 
 export const Map = (props: Props) => {
-    const position: LatLngExpression = [51.505, -0.09]
+    const pos = useRecoilValue(position)
+    const teig  = useRecoilValueLoadable(selectedTeig)
     const apikey = useRecoilValue(apiKey)
-    console.log(apikey)
+
 
     return (
         <div>
+
             {apikey &&
-                <MapContainer style={{ height: '100vh', width: '100vw' }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <MapContainer style={{ height: '100vh', width: '100vw' }} center={pos} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
                     url={`https://waapi.webatlas.no/maptiles/tiles/webatlas-orto-newup/wa_grid/{z}/{x}/{y}.jpeg?APITOKEN=${apikey}`}
                 />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-                </Marker>
+                
             </MapContainer>}
+
         </div>
 
     )
