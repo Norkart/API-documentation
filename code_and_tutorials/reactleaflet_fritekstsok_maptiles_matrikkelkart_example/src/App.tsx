5c158b887da203a8ffda7722823ce8c7660bdc9d
Petter Sellebakk Bjørkås <petbjo@norkart.no>
2021-06-10 13:38:53 +0200

    add ApiKey component

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/App.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ApiKey } from './components/ApiKey'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ApiKey />
      content here
     </div>
   );
