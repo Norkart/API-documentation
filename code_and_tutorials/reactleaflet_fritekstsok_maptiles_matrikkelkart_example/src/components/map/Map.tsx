3c443d0e93538a3dca9d800c771d4c35c7d3e4c7
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-14 18:01:54 +0200

    move search disable map scroll and click propagation through search field

---
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/map/Map.tsx
@@ -0,0 +1,39 @@
+import { Suspense } from "react"
+import { MapContainer, TileLayer, ZoomControl } from "react-leaflet"
+import { useRecoilValue } from "recoil"
+import { apiKey, selectedAddress} from "../../state/state"
+import { MapLayers } from "../MapLayers"
+import { TopLeftMapControl } from "./TopLeftMapControl"
+
+
+interface Props {
+
+}
+
+export const Map = (props: Props) => {
+    const address = useRecoilValue(selectedAddress);
+    const apikey = useRecoilValue(apiKey);
+
+    const latlngOrDefault = address ? address.latlng : { lat: 63.426891, lng: 10.396416}; 
+
+    return (
+        <div>
+            {apikey &&
+                <MapContainer style={{ height: '100vh', width: '100vw' }} center={latlngOrDefault} zoomControl={false} zoom={16} scrollWheelZoom={true}>
+                    <Suspense fallback={<div></div>}>
+                        <TileLayer
+                            url={`https://waapi.webatlas.no/maptiles/tiles/webatlas-standard-hybrid/wa_grid/{z}/{x}/{y}.jpeg?APITOKEN=${apikey}`}
+                            attribution={`Norkart`}
+                        />
+                        <MapLayers/>
+
+                        <TopLeftMapControl/>
+                        <ZoomControl position='bottomright'/>
+
+                    </Suspense>
+                </MapContainer>}
+        </div>
+    )
+}
+
+
