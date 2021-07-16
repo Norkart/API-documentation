82f8cc945875948aa41915556d6c1948f172aefd
Kari Meling Johannessen <karjoh@norkart.no>
2021-07-16 17:59:16 +0200

    test

--- code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/map/Map.tsx
+++ code_and_tutorials/reactleaflet_fritekstsok_maptiles_matrikkelkart_example/src/components/map/Map.tsx
@@ -1,4 +1,5 @@
-import { Suspense } from "react"
+import { throttle } from "lodash"
+import { Suspense, useEffect, useState } from "react"
 import { MapContainer, ZoomControl } from "react-leaflet"
 import { useRecoilValue } from "recoil"
 import { apiKey } from "../../state/state"
@@ -9,10 +10,22 @@ import { TopLeftMapControl as SearchControl } from "./SearchControl"
 
 export const Map = () => {
     const apikey = useRecoilValue(apiKey);
-    const center = { lat: 63.5, lng: 10.5};
+    const center = { lat: 63.5, lng: 10.5}; 
+    const [mapHeight, setmapHeight] = useState<'100vh' | number>('100vh') //hack for full height in mobile browsers.
+
+    const onResize = throttle(() => {
+        setmapHeight(window.innerHeight);
+    }, 200)
+
+    useEffect(() => {
+        window.addEventListener('resize', onResize);
+        return () => {
+            window.removeEventListener('resize', onResize);
+        }
+    }, [])
 
     return (
-        <div style={{ height: '100%' }} >
+        <div style={{ height: mapHeight}} >
             {apikey &&
                 <MapContainer style={{ height: '100%', width: '100vw' }} center={center} zoomControl={false} zoom={5} scrollWheelZoom={true}>
                     <Suspense fallback={<div></div>}>
